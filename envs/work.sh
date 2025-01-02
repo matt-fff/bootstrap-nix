76987d2d010e57612420e766378450572aad804a
Matt White <mail@matt-w.net>
2025-01-01 19:08:41 -0700

    tmp

--- envs/work.sh
+++ envs/work.sh
@@ -3,3 +3,4 @@
 export HOSTNAME="${HOSTNAME:-work}"
 export EXTRA_PACKAGES=""
 export CONFIG_EXCLUSIONS="nas-configuration.nix"
+export NIX_HARDWARE="nixos-hardware.nixosModules.lenovo-legion-16irx9h"
