cd12cf2670569efe3f8dd79e9e9fb1de0b39e6d4
Matthew White <mail@matt6f.com>
2025-02-16 21:22:55 -0700

    tweaks

--- bs-3-finalize.sh
+++ bs-3-finalize.sh
@@ -32,15 +32,10 @@ if [ "${LINUX_TYPE}" == "arch" ]; then
   yay -Sy --noconfirm --sudoloop \
     asdf-vm \
     sapling-scm-bin \
-<<<<<<< Updated upstream
     xrdp \
-    gnome-remote-desktop
-||||||| Stash base
-    xrdp
-=======
+    gnome-remote-desktop \
     xrdp \ 
     cifs-utils
->>>>>>> Stashed changes
 
   sudo systemctl enable --now xrdp
 
