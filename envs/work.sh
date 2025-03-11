526f0f82df3c8ba426cbb457c12bf5c4a3c70f7b
Matthew White <mail@matt6f.com>
2025-03-10 20:55:47 -0600

    work

--- envs/work.sh
+++ envs/work.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
 
 export HOSTNAME="${HOSTNAME:-work}"
-export CONFIG_EXCLUSIONS="nas-configuration.nix"
 export ADDITIONAL_INPUTS="nixos-hardware.url = \"nixos-hardware/master\";"
 export ADDITIONAL_OUTPUTS="nixos-hardware,"
 export ADDITIONAL_MODULES="nixos-hardware.nixosModules.lenovo-legion-16irx9h"
+
+export LOCALE=""
